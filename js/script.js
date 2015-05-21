c9c311cad90273e892184965b768780d73e4fa23
boykiv <boykiv@hotmail.com>
2015-05-21 19:14:56 +0300

    Add modal

--- js/script.js
+++ js/script.js
@@ -48,4 +48,12 @@
 		$('.main-menu li').removeClass('active');
 		element.addClass('active');
 	}
+
+	$('.projects-list a').on('click', function () {
+		$('.overlay').show();
+	});
+
+	$('.modal .close').on('click', function () {
+		$('.overlay').hide();
+	});
 })();
