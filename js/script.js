cca17a848bdf750676442ee698cb9b48857d7d1c
boykiv <boykiv@hotmail.com>
2015-05-21 17:16:10 +0300

    Navigation

--- js/script.js
+++ js/script.js
@@ -1,121 +1,46 @@
 (function(){
-	"use strict";
+	"use strict"
 
-	var bigDay = new Date(2015, 5, 1);
+	var sections = [
+		{
+			menuItem: $('.home-link'),
+			element: $('.slogan')
+		},
+		{
+			menuItem: $('.services-link'),
+			element: $('.areas')
+		},
+		{
+			menuItem: $('.projects-link'),
+			element: $('.projects')
+		},
+		{
+			menuItem: $('.contacts-link'),
+			element: $('.contacts')
+		}
+	];
 
-	setInterval(check, 100);
+	sections[0].menuItem.on('click', function() {
+		console.log(0)
+		scrollToEl(sections[0].element);
+	});
+	sections[1].menuItem.on('click', function() {
+		console.log(1)
+		scrollToEl(sections[1].element);
+	});
+	sections[2].menuItem.on('click', function() {
+		console.log(2)
+		scrollToEl(sections[2].element);
+	});
+	sections[3].menuItem.on('click', function() {
+		console.log(3)
+		scrollToEl(sections[3].element);
+	});
+	
 
-	function tick(target, val) {
-		$(target).html('<div class="num animated fadeOutDown">'+(val+1)%10+'</div><div class="num animated fadeInDown">'+val+'</div>');
+	function scrollToEl(element) {
+		$('html, body').animate({
+			scrollTop: ($(element).first().offset().top - 66)
+		},500);
 	}
-
-	function getSeconds(time) {
-		return Math.floor((time % (60 * 1000)) / (1000));
-	}
-
-	function getMinutes(time) {
-		return Math.floor((time % (3600 * 1000)) / (60 * 1000));
-	}
-
-	function getHours(time) {
-		return Math.floor((time % (3600 * 24 * 1000)) / (3600 * 1000));
-	}
-
-	function getDays(time) {
-		return Math.floor((time)/(3600 * 24 * 1000));
-	}
-
-	function check() {
-		var now = Date.now();
-
-		var bigDay = new Date(2015, 5, 1);
-
-		var distance = bigDay - now;
-
-		var days = getDays(distance);
-		var hours = getHours(distance);
-		var minutes = getMinutes(distance);
-		var seconds = getSeconds(distance);
-
-		var daysNew1 = Math.floor(days / 10);
-		var daysNew2 = days % 10;
-
-		var hoursNew1 = Math.floor(hours / 10);
-		var hoursNew2 = hours % 10;
-
-		var minutesNew1 = Math.floor(minutes / 10);
-		var minutesNew2 = minutes % 10;
-
-		var secondsNew1 = Math.floor(seconds / 10);
-		var secondsNew2 = seconds % 10;
-
-		if (daysNew1 !== days1) {
-			tick('.days1', daysNew1);
-			days1 = daysNew1;
-		}
-
-		if (daysNew2 !== days2) {
-			tick('.days2', daysNew2);
-			days2 = daysNew2;
-		}
-
-		if (hoursNew1 !== hours1) {
-			tick('.hours1', hoursNew1);
-			hours1 = hoursNew1;
-		}
-
-		if (hoursNew2 !== hours2) {
-			tick('.hours2', hoursNew2);
-			hours2 = hoursNew2;
-		}
-
-		if (minutesNew1 !== minutes1) {
-			tick('.minutes1', minutesNew1);
-			minutes1 = minutesNew1;
-		}
-
-		if (minutesNew2 !== minutes2) {
-			tick('.minutes2', minutesNew2);
-			minutes2 = minutesNew2;
-		}
-
-		if (secondsNew1 !== seconds1) {
-			tick('.seconds1', secondsNew1);
-			seconds1 = secondsNew1;
-		}
-
-		if (secondsNew2 !== seconds2) {
-			tick('.seconds2', secondsNew2);
-			seconds2 = secondsNew2;
-		}
-	}
-
-	var now = Date.now();
-
-	var distance = bigDay - now;
-
-	var days = getDays(distance);
-	var hours = getHours(distance);
-	var minutes = getMinutes(distance);
-
-	var days1 = Math.floor(days / 10);
-	var days2 = days % 10;
-
-	var hours1 = Math.floor(hours / 10);
-	var hours2 = hours % 10;
-
-	var minutes1 = Math.floor(minutes / 10);
-	var minutes2 = minutes % 10;
-
-	var seconds1 = Math.floor(seconds / 10);
-	var seconds2 = seconds % 10;
-
-	$('.days1 .num').html(days1);
-	$('.days2 .num').html(days2);
-	$('.hours1 .num').html(hours1);
-	$('.hours2 .num').html(hours2);
-	$('.minutes1 .num').html(minutes1);
-	$('.minutes2 .num').html(minutes2);
-	$('.seconds1 .num').html(seconds1);
-	$('.seconds2 .num').html(seconds2);
 })();
