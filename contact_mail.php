bfdfed65733d91da68e6b3da90e34ab93d5fd393
boykiv <boykiv@hotmail.com>
2015-05-29 18:48:25 +0300

    Email functionality

---
+++ contact_mail.php
@@ -0,0 +1,9 @@
+<?php
+$toEmail = "hack4tourism@yandex.ru";
+$mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+print_r("<p class='success'>Contact Mail Sent.</p>");
+} else {
+print_r("<p class='Error'>Problem in Sending Mail.</p>");
+}
+?>
