f444d34dee7d2a14c047524dd5b39413747da493
boykiv <boykiv@hotmail.com>
2015-06-01 13:42:08 +0300

    Project is cleaned

--- js/script.js
+++ js/script.js
@@ -106,4 +106,62 @@ $(document).alton({
 	}
 	resizeVideo();
 
+
+	// FORM SENDING
+
+	$('#frmContact .submit').on('click', sendContact);
+
+	function sendContact() {
+		var valid;
+		valid = validateContact();
+		if(valid) {
+			jQuery.ajax({
+				url: "contact_mail.php",
+				data:'userName='+$("#firstName").val()+' '+$("#lastName").val()+'&userEmail='+$("#userEmail").val()+'&subject='+$("#subject").val()+'&content=('+$('#phone').val()+')'+$('#content').val(),
+				type: "POST",
+				success:function(data){
+					swal("You just sent your email", "We will answer you in 24 hours", "success")
+				},
+				error:function (){}
+			});
+		}
+	}
+
+	function validateContact() {
+		var valid = true;
+		$("#frmContact > *").removeClass('invalid');
+		$(".info").html('');
+
+		if(!$("#firstName").val()) {
+			$("#firstName").addClass('invalid');
+			valid = false;
+		}
+		if(!$("#lastName").val()) {
+			$("#lastName").addClass('invalid');
+			valid = false;
+		}
+		if(!$("#userEmail").val()) {
+			$("#userEmail").addClass('invalid');
+			valid = false;
+		}
+		if(!$("#phone").val()) {
+			$("#phone").addClass('invalid');
+			valid = false;
+		}
+		if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+			$("#userEmail").addClass('invalid');
+			valid = false;
+		}
+		if(!$("#subject").val()) {
+			$("#subject").addClass('invalid');
+			valid = false;
+		}
+		if(!$("#content").val()) {
+			$("#content").addClass('invalid');
+			valid = false;
+		}
+
+		return valid;
+	}
+
 })();
